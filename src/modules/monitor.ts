f2ae1a4159836cd3d0e9ca30c471b4b33bb2ba6e
zen0 <79776344+zen0dev@users.noreply.github.com>
2022-03-28 07:53:55 +0100

    build

--- src/modules/monitor.ts
+++ src/modules/monitor.ts
@@ -1,13 +1,21 @@
 const os = require('os')
-
+const fs = require('fs')
 
 export function initMonitor(app) {
   let logs = []
 
   setInterval(function() {
-    if (os.freemem() / 1024 / 1024 < 200) {
+    const available = Number(/MemAvailable:[ ]+(\d+)/.exec(fs.readFileSync('/proc/meminfo', 'utf8'))[1]) / 1024
+
+    if (available < 200) {
       if (logs.length >= 5) {
-        console.log('Memory too low', os.freemem() / os.totalmem())
+        const free = os.freemem() / 1024 / 1024
+        const total = os.totalmem() / 1024 / 1024
+        
+        console.log('Free mem', free)
+        console.log('Available mem', available)
+        console.log('Total mem', total)
+
         process.exit()
       }
     } else {
@@ -16,10 +24,14 @@ export function initMonitor(app) {
   }, 60 * 1000)
 
   setInterval(function() {
-    console.log('Free mem', os.freemem() / 1024 / 1024)
-    console.log('Total mem', os.totalmem() / 1024 / 1024)
-    if (os.freemem() / 1024 / 1024 < 200) { // if ((os.freemem() / os.totalmem()) < 0.2) {
-      console.log('Memory flagged', os.freemem() / os.totalmem())
+    // const free = os.freemem() / 1024 / 1024
+    const available = Number(/MemAvailable:[ ]+(\d+)/.exec(fs.readFileSync('/proc/meminfo', 'utf8'))[1]) / 1024
+    // const total = os.totalmem() / 1024 / 1024
+    // console.log('Free mem', free)
+    // console.log('Available mem', available)
+    // console.log('Total mem', total)
+    if (available < 200) { // if ((os.freemem() / os.totalmem()) < 0.2) {
+      console.log('Memory flagged', available)
       logs.push(true)
     }
   }, 10 * 1000)
