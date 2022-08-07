9d68ca8e96c448fa103ec4c7ee04463ecc0c014f
Binzy <xbinzy@protonmail.com>
2022-08-07 17:57:20 +0100

    build: Binzy doz it

--- game-server/src/modules/game-server.ts
+++ game-server/src/modules/game-server.ts
@@ -946,8 +946,8 @@ const registerKill = (app, winner, loser) => {
   const notReallyTrying = config.antifeed1 ? (totalKills >= 2 && loser.kills < 2 && loser.rewards <= 1) || (totalKills >= 2 && loser.kills < 2 && loser.powerups <= 100) : false
   const tooManyKills = config.antifeed2 ? clients.length > 2 && totalKills >= 5 && totalKills > winner.log.kills.length / clients.filter(c => !c.isDead).length : false
   const killingThemselves = config.antifeed3 ? winner.hash === loser.hash : false
-  const allowKill = !notReallyTrying && !tooManyKills // && !killingThemselves
-    
+  const allowKill = !notReallyTrying && !tooManyKills && !pandas.includes(winner.address)// && !killingThemselves
+  console.log(winner.address, pandas, config.gameMode)
   if (notReallyTrying) {
     loser.log.notReallyTrying += 1
   }
@@ -963,7 +963,7 @@ const registerKill = (app, winner, loser) => {
 
     return
   }
-console.log(winner.address, pandas, config.gameMode)
+
   if (config.gameMode === 'Pandamonium' && !pandas.includes(winner.address)) {
     return
   }
