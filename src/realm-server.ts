00cc5e965dcd83f3ff2eb983c20b24c1b3302199
zen0 <79776344+zen0dev@users.noreply.github.com>
2024-12-25 00:20:27 -0800

    cert

--- src/realm-server.ts
+++ src/realm-server.ts
@@ -93,8 +93,8 @@ export class RealmServer implements Realm.Service {
     if (this.isHttps) {
       this.https = require('https').createServer(
         {
-          key: fs.readFileSync(path.resolve('./privkey.pem')),
-          cert: fs.readFileSync(path.resolve('./fullchain.pem')),
+          key: fs.readFileSync('/etc/letsencrypt/live/hoff.arken.gg/privkey.pem'), //fs.readFileSync(path.resolve('./privkey.pem')),
+          cert: fs.readFileSync('/etc/letsencrypt/live/hoff.arken.gg/fullchain.pem'), // fs.readFileSync(path.resolve('./fullchain.pem')),
         },
         this.server
       );
